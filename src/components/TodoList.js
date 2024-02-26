35691e95d77d7e69eb01d72d3ec8b5c5e6110224
GenaPlem <shainkill1337@gmail.com>
2024-02-26 14:45:26 +0000

    Add handlers to control Updating, Additing and Deleting of Todo Items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,37 @@
 import {useState} from "react";
 import {TodoItem} from "./TodoItem";
+import {TodoComposer} from "./TodoComposer";
 
 export const TodoList = () => {
     const [todos, setTodos] = useState([{id: 1, value: 'Learn React', completed: false}])
 
+    const handleUpdateTodo = (updatedTodo) => {
+        const newTodos = todos.map(todo =>
+            todo.id === updatedTodo.id ? updatedTodo : todo
+        )
+        setTodos(newTodos)
+    }
+
+    const handleDeleteTodo = (id) => {
+        const newTodos = todos.filter(todo => todo.id !== id)
+        setTodos(newTodos)
+    }
+
+    const handleAddTodo = (newTodo) => {
+        const newTodos = [...todos, newTodo]
+        setTodos(newTodos)
+    }
+
     return (
         <div className='todo-list__wrapper'>
+            <TodoComposer handleAddTodo={handleAddTodo}/>
+            <h3 className='todo-list__title'>Todo List</h3>
             <ul className='todo-list'>
-                {todos.map(todo => <TodoItem key={todo.id} value={todo.value} completed={todo.completed}/>)}
+                {todos.length !== 0
+                    ? todos.map(todo => <TodoItem todo={todo}
+                                                  handleUpdateTodo={handleUpdateTodo}
+                                                  handleDeleteTodo={handleDeleteTodo}/>)
+                    : <li className='todo-list__info'>Your list is empty</li>}
             </ul>
         </div>
     )
