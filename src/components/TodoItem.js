bcf80e4c72065c35569f097024abad235497abf2
GenaPlem <shainkill1337@gmail.com>
2024-02-26 14:44:43 +0000

    Add handler to update Todo Checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,20 @@
-export const TodoItem = ({key, value, completed}) => {
+export const TodoItem = ({todo, handleUpdateTodo}) => {
+
+    const handleCheckboxClick = () => handleUpdateTodo({
+        ...todo,
+        completed: !todo.completed
+    })
 
     return (
         <li className='todo-list__item'>
-            <label htmlFor={key}>
-                <input className='todo-list__item-checkbox' type="checkbox" name={key} checked={completed} id={key}/>
-                <span className='todo-list__item-value'>{value}</span>
-                <button type={"button"} className='todo-list__item-delete'>Delete</button>
-                <button type={"button"} className='todo-list__item-edit'>Edit</button>
+            <label htmlFor={todo.id}>
+                <input className='todo-list__item-checkbox' type="checkbox" name={todo.id} checked={todo.completed}
+                       onChange={handleCheckboxClick} id={todo.id}/>
+                <span
+                    className={todo.completed ? 'todo-list__item-value completed' : 'todo-list__item-value'}>{todo.value}</span>
             </label>
+            <button type={"button"} className='todo-list__item-delete'>Delete</button>
+            <button type={"button"} className='todo-list__item-edit'>Edit</button>
         </li>
     )
 }
