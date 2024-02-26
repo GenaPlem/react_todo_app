84ed52313367c6512f3437ee5cb7ec0ece3e2963
GenaPlem <shainkill1337@gmail.com>
2024-02-26 14:44:16 +0000

    Add handlers to create new Todo Item

--- src/components/TodoComposer.js
+++ src/components/TodoComposer.js
@@ -1,9 +1,27 @@
+import {useState} from "react";
+import {createTodo} from "../utilities/createTodo";
+
 export const TodoComposer = ({handleAddTodo}) => {
+
+    const [todoValue, setTodoValue] = useState('')
+
+    const handleUpdateTodo = (e) => {
+        setTodoValue(e.target.value)
+    }
+
+    const handleAddTodoClick = () => {
+        const todo = createTodo(todoValue)
+        handleAddTodo(todo)
+        setTodoValue('')
+    }
 
     return (
         <>
-            <input type={"text"} className='todo-list__input' placeholder={"Enter your todo here"}/>
-            <button type={"button"} className='todo-list__add-btn'>Add Todo</button>
+            <input type={"text"} className='todo-list__input' value={todoValue} onChange={handleUpdateTodo}
+                   placeholder={"Enter your todo here"}/>
+            <button type={"button"} disabled={todoValue.length === 0} onClick={handleAddTodoClick}
+                    className='todo-list__add-btn'>Add Todo
+            </button>
         </>
     )
 }
