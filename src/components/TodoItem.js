d6656ee986a31814e64a4beed5dde333232de148
GenaPlem <shainkill1337@gmail.com>
2024-02-26 14:03:22 +0000

    Add edit and delete buttons to item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,6 +5,8 @@ export const TodoItem = ({key, value, completed}) => {
             <label htmlFor={key}>
                 <input className='todo-list__item-checkbox' type="checkbox" name={key} checked={completed} id={key}/>
                 <span className='todo-list__item-value'>{value}</span>
+                <button type={"button"} className='todo-list__item-delete'>Delete</button>
+                <button type={"button"} className='todo-list__item-edit'>Edit</button>
             </label>
         </li>
     )
